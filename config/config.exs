a1aa95f0d0f245d473279ae442d28c231d22804b
kratos2377 <shobityadav23@gmail.com>
2024-11-30 13:35:17 +0530

    Feat: KAfka and Postgres COnfig fix:

--- config/config.exs
+++ config/config.exs
@@ -1,32 +1,33 @@
 import Config
 
+database_url =
+  System.get_env("DATABASE_URL") ||
+    "postgres://postgres:secret@localhost:5432/vortex"
 
-# config :kafka_ex,
-# brokers: [
-#   {"localhost", 9092},
-#   {"localhost", 9093},
-#   {"localhost", 9094}
+config :kafka_ex,
+brokers: [
+  {"localhost", 9092}
+],
+consumer_group: "kafka_ex",
+client_id: "kafka_ex",
+sync_timeout: 3000,
+max_restarts: 5,
+max_seconds: 30,
+commit_interval: 5_000,
+commit_threshold: 100,
+auto_offset_reset: :none,
+sleep_for_reconnect: 600,
+use_ssl: false,
+# ssl_options: [
+#   # Fix warnings. More at https://github.com/erlang/otp/issues/5352
+#   verify: :verify_none,
+#   cacertfile: File.cwd!() <> "/ssl/ca-cert",
+#   certfile: File.cwd!() <> "/ssl/cert.pem",
+#   keyfile: File.cwd!() <> "/ssl/key.pem"
 # ],
-# consumer_group: "kafka_ex",
-# client_id: "kafka_ex",
-# sync_timeout: 3000,
-# max_restarts: 5,
-# max_seconds: 30,
-# commit_interval: 5_000,
-# commit_threshold: 100,
-# auto_offset_reset: :none,
-# sleep_for_reconnect: 600,
-# use_ssl: false,
-# # ssl_options: [
-# #   # Fix warnings. More at https://github.com/erlang/otp/issues/5352
-# #   verify: :verify_none,
-# #   cacertfile: File.cwd!() <> "/ssl/ca-cert",
-# #   certfile: File.cwd!() <> "/ssl/cert.pem",
-# #   keyfile: File.cwd!() <> "/ssl/key.pem"
-# # ],
-# snappy_module: :snappyer,
-# kafka_version: "0.10.1"
-# env_config = Path.expand("#{Mix.env()}.exs", __DIR__)
+snappy_module: :snappyer,
+kafka_version: "0.10.1"
+env_config = Path.expand("#{Mix.env()}.exs", __DIR__)
 
 
 config :vortex_pub_sub, VortexPubSub.MongoRepo,
@@ -36,13 +37,7 @@ config :vortex_pub_sub, VortexPubSub.MongoRepo,
   idle_interval: 10_000,
   queue_target: 5_000
 
-config :vortex_pub_sub, VortexPubSub.PostgresRepo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "dbname_dev",
-  hostname: "localhost",
-  pool_size: 5
+config :vortex_pub_sub, VortexPubSub.PostgresRepo, url: database_url
 
 # if File.exists?(env_config) do
 #   import_config(env_config)
--- lib/postgres_repo.ex
+++ lib/postgres_repo.ex
@@ -1,5 +1,5 @@
 defmodule VortexPubSub.PostgresRepo do
   use Ecto.Repo,
-    otp_app: :myapp,
+    otp_app: :vortex_pub_sub,
     adapter: Ecto.Adapters.Postgres
 end
